48f75f9a09b01bd7e0aff87bc65e4d438dafd835
ds-praveenkumar <praveenkr2208@gmail.com>
2023-03-17 00:36:22 +0530

    add rules

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -20,19 +20,20 @@ provider "azurerm" {
 # }
 
 # Create the network VNET
-# resource "azurerm_virtual_network" "revel-vnet" {
-#   name                = "network-vnet" 
-#   address_space       = [var.network-vnet-cidr]
-#   resource_group_name = data.azurerm_resource_group.resource-group.name  #azurerm_resource_group.network-rg.name
-#   location            = var.location
-# }
+resource "azurerm_virtual_network" "reval-vnet" {
+  name                = "network-vnet" 
+  address_space       = [var.network-vnet-cidr]
+  resource_group_name = data.azurerm_resource_group.resource-group.name  #azurerm_resource_group.network-rg.name
+  location            = var.location
+}
 
 # Create a subnet for endpoints
-resource "azurerm_subnet" "revel-sn" {
-  name                 = "revel-subnet"
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
+resource "azurerm_subnet" "reval-sn" {
+  name                 = "reval-subnet"
+  virtual_network_name = azurerm_virtual_network.reval-vnet.name
   resource_group_name  = data.azurerm_resource_group.resource-group.name  #azurerm_resource_group.network-rg.name
   address_prefixes     = [var.endpoint-subnet-cidr]
+  service_endpoints = ["Microsoft.Storage"]
 }
 
 
@@ -43,32 +44,49 @@ resource "azurerm_private_dns_zone" "dns-zone" {
 }
 
 # Create Storage Account
-resource "azurerm_storage_account" "revel-asa" {
-  name                = "revelasa"
+resource "azurerm_storage_account" "reval-asa" {
+  name                = "revalasa"
   resource_group_name = data.azurerm_resource_group.resource-group.name  #azurerm_resource_group.network-rg.name
   location            = var.location
 
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+
+    network_rules {
+        bypass = ["Logging", "Metrics", "AzureServices"]
+        default_action = "Deny"
+        virtual_network_subnet_ids = [azurerm_subnet.reval-sn.id]
+    }
+    
+    depends_on = [
+	azurerm_virtual_network.reval-vnet,
+	azurerm_subnet.reval-sn
+  ]
+
 }
 
 resource "azurerm_storage_share" "file-share" {
   name                 = "ravelfs"
-  storage_account_name = azurerm_storage_account.revel-asa.name
+  storage_account_name = azurerm_storage_account.reval-asa.name
   quota                = 50
+
+  depends_on =[
+    azurerm_storage_account.reval-asa
+  ]
 }
 
 # Create Private Endpoint
 resource "azurerm_private_endpoint" "endpoint" {
-  name                = "revel_endpoint"
+  name                = "reval_endpoint"
   resource_group_name = data.azurerm_resource_group.resource-group.name  #azurerm_resource_group.network-rg.name
   location            = var.location
-  subnet_id           = azurerm_subnet.revel-sn.id
+  subnet_id           = azurerm_subnet.reval-sn.id
 
   private_service_connection {
     name                           = "ravel_psc"
-    private_connection_resource_id = azurerm_storage_account.revel-asa.id
+    private_connection_resource_id = azurerm_storage_account.reval-asa.id
     is_manual_connection           = false
     subresource_names              = ["blob"]
   }
@@ -83,3 +101,11 @@ resource "azurerm_private_dns_a_record" "dns_a" {
   records             = [azurerm_private_endpoint.endpoint.private_service_connection.0.private_ip_address]
 }
 
+# resource "azurerm_storage_account_network_rules" "nwrules" {
+#   storage_account_id = azurerm_storage_account.reval-asa.id
+
+#   default_action             = "Allow"
+#   ip_rules                   = ["127.0.0.1"]
+#   virtual_network_subnet_ids = [azurerm_subnet.reval-sn.id]
+#   bypass                     = ["Metrics"]
+# }
--- terraform-azure/outputs.tf
+++ terraform-azure/outputs.tf
@@ -1,17 +1,17 @@
 
 # Outputs
 output "storage_account_name" {
-  value = azurerm_storage_account.revel-asa.name
+  value = azurerm_storage_account.reval-asa.name
 }
 
 output "storage_account_id" {
-  value = azurerm_storage_account.revel-asa.id
+  value = azurerm_storage_account.reval-asa.id
 }
 
-output "storage_dns_a_record" {
-  value = azurerm_private_dns_a_record.dns_a.fqdn
-}
+# output "storage_dns_a_record" {
+#   value = azurerm_private_dns_a_record.dns_a.fqdn
+# }
 
-output "storage_ip_address" {
-  value = azurerm_private_endpoint.endpoint.private_service_connection.0.private_ip_address
-}
+# output "storage_ip_address" {
+#   value = azurerm_private_endpoint.endpoint.private_service_connection.0.private_ip_address
+# }
