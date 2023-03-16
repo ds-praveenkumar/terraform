708346c33cb3057a0b77ddcdd8161ac0315e9ff3
ds-praveenkumar <praveenkr2208@gmail.com>
2023-03-16 10:28:27 +0530

    add variables.tf

--- main.tf
+++ main.tf
@@ -10,16 +10,16 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name         = "nginx"
+  name         = var.docker_image
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = var.container_name
 
   ports {
-    internal = 80
-    external = 8000
+    internal = var.internal_port
+    external = var.external_port
   }
 }
